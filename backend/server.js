475bf34c7e3766a92cc16c449ed45fa38177eaa1
Eraj Tanweer <erajtanweer2@gmail.com>
2025-02-14 15:37:36 +0500

    abc

--- backend/server.js
+++ backend/server.js
@@ -12,8 +12,8 @@ app.options('*', cors()); // Allow preflight requests
 
 // Middleware to enable CORS
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://iba-hackathon.vercel.app'); // Adjust the origin as needed
-  // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173'); 
+  // res.setHeader('Access-Control-Allow-Origin', 'https://iba-hackathon.vercel.app'); // Adjust the origin as needed
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173'); 
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true'); 
